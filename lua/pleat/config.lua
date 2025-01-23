3f86ff536099e0c91362d1de13f7e8de9c418295
Tom Bell <tomb@tombell.dev>
2025-01-23 19:03:39 +0000

    wip

---
+++ lua/pleat/config.lua
@@ -0,0 +1,34 @@
+---@class pleat.Config.mod: pleat.Config
+local M = {}
+
+---@class pleat.Config
+---@field fill_char string
+---@field collapsed_char string
+---@field foldend_exclude_filetypes string[]
+local defaults = {
+  fill_char = " ",
+  collapsed_char = "…",
+  foldend_exclude_filetypes = {
+    "markdown",
+    "python",
+    "yaml",
+  },
+}
+
+---@type pleat.Config
+local options
+
+---@param opts? pleat.Config
+function M.setup(opts)
+  ---@type pleat.Config
+  options = vim.tbl_deep_extend("force", {}, options or defaults, opts or {})
+
+  return options
+end
+
+return setmetatable(M, {
+  __index = function(_, key)
+    options = options or M.setup()
+    return options[key]
+  end,
+})
